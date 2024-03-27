ab2356611be664ea4e4dc1cb6a5c43a734cee4c0
desairudra <desairudra4366@gmail.com>
2024-03-27 14:56:29 +0530

    prints all wrong words and add to dictonary option

--- capstone/source_code.cpp
+++ capstone/source_code.cpp
@@ -6,39 +6,59 @@ int main()
     FILE *fptr1=fopen("newword.txt","r");
     FILE *fptr=fopen("dictonary.txt","r");
     map<string,int>word_checker;
-    string p;
+    int k=0,f=0;
     char s2[1000];
     while(fscanf(fptr,"%s",s2)!=EOF)
     { 
       word_checker[s2]=1;
     }
     vector<string>Wrong_words;
-     while(fscanf(fptr1,"%s",s1)!=EOF)
-    { 
+    while(fscanf(fptr1,"%s",s1)!=EOF)
+    { if(s1[strlen(s1)-1]=='.'||s1[strlen(s1)-1]=='!'||s1[strlen(s1)-1]=='?'||s1[strlen(s1)-1]==','||s1[strlen(s1)-1]==';'||s1[strlen(s1)-1]==':')
+      s1[strlen(s1)-1]='\0';
       if(!(word_checker[s1]))
       Wrong_words.push_back(s1);
        
     }
-    
     fclose(fptr);
-    
     fptr=fopen("dictonary.txt","a");
-    for(int i=0;i<Wrong_words.size();i++)
+    for(auto s:Wrong_words)
     { 
+     
       
-      cout<<"add "<<Wrong_words[i]<<" to dictonary"<<endl;
-      string s3;
-      cin>>s3;
-      for(int j=0;j<Wrong_words[i].size();j++)
-      s1[j]=Wrong_words[i][j];
-      s1[Wrong_words[i].size()]='\0';
-      
-      if(s3=="yes")
-      {fprintf(fptr,"%s\n",s1);}
+     m: for(int i=0;s[k]!='.'&&s[k]!='?'&&s[k]!=','&&k<s.size()&&s[k]!='!';i++)
+      {s1[i]=s[k];
+       k++;
+       f=i;
+      }
+      s1[f+1]='\0';
+      if(!word_checker[s1])
+      { cout<<"add "<<s1<<" to dictonary"<<endl; 
+       string s3;
+       cin>>s3;
+       if(s3=="yes")
+       { fprintf(fptr,"%s ",s1);
+         word_checker[s1]=1;
+       }
+       k++;
+       if(k<s.size())
+       goto m;
+       else
+       k=0;
+      }
+      else
+      {
+          k++;
+         
+          if(k<s.size())
+          goto m;
+          else
+          k=0;
+      }
+    }
       
     
-    }
-    cout<<"Exit"<<endl;
+    
     fclose(fptr);
     fclose(fptr1);
     return 0; 
